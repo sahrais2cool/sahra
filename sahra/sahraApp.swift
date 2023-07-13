498df84f7b8abf06c558b329f26a982b9e540ad9
sahrais2cool <sahravann0@gmail.com>
2023-07-13 13:06:46 -0700

    Initial Commit

---
+++ sahra/sahraApp.swift
@@ -0,0 +1,17 @@
+//
+//  sahraApp.swift
+//  sahra
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct sahraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
